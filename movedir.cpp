f3c9226110eb8500f6e3dd825fa1626b049eef1e
nmcarp99 <36648995+nmcarp99@users.noreply.github.com>
2020-05-15 15:25:16 -0400

    Add files via upload

--- movedir.cpp
+++ movedir.cpp
@@ -3,36 +3,46 @@
 
 increments checkIncrements(float x, float y, float destinationX, float destinationY) {
 	increments incrementVals = { 0, 0 };
-	if (destinationX > x && destinationY > y) {
-		incrementVals.x = (destinationX - x) / (destinationY - y);
-		incrementVals.y = 1;
-		while (incrementVals.x > 3) {
-			incrementVals.x = incrementVals.x / 2;
-			incrementVals.y = incrementVals.y / 2;
+	if (destinationY > y+5) {
+		incrementVals = { 0, 1 };
+	}
+	else if (destinationY < y-5) {
+		incrementVals = { 0, -1 };
+	}
+	if (destinationX > x+5) {
+		if (destinationY > y) {
+			incrementVals.x = (destinationX - x) / (destinationY - y);
+			incrementVals.y = 1;
+			while (incrementVals.x > 3) {
+				incrementVals.x = incrementVals.x / 2;
+				incrementVals.y = incrementVals.y / 2;
+			}
+		}
+		else if (destinationY < y) {
+			incrementVals.x = -((destinationX - x) / (destinationY - y));
+			incrementVals.y = -1;
+			while (incrementVals.x > 3) {
+				incrementVals.x = incrementVals.x / 2;
+				incrementVals.y = incrementVals.y / 2;
+			}
 		}
 	}
-	else if (destinationX < x && destinationY > y) {
-		incrementVals.x = (destinationX - x) / (destinationY - y);
-		incrementVals.y = 1;
-		while (incrementVals.x < -3) {
-			incrementVals.x = incrementVals.x / 2;
-			incrementVals.y = incrementVals.y / 2;
+	else if (destinationX < x-5) {
+		if (destinationY > y) {
+			incrementVals.x = (destinationX - x) / (destinationY - y);
+			incrementVals.y = 1;
+			while (incrementVals.x < -3) {
+				incrementVals.x = incrementVals.x / 2;
+				incrementVals.y = incrementVals.y / 2;
+			}
 		}
-	}
-	else if (destinationX < x && destinationY < y) {
-		incrementVals.x = -((destinationX - x) / (destinationY - y));
-		incrementVals.y = -1;
-		while (incrementVals.x < -3) {
-			incrementVals.x = incrementVals.x / 2;
-			incrementVals.y = incrementVals.y / 2;
-		}
-	}
-	else if (destinationX > x && destinationY < y) {
-		incrementVals.x = -((destinationX - x) / (destinationY - y));
-		incrementVals.y = -1;
-		while (incrementVals.x > 3) {
-			incrementVals.x = incrementVals.x / 2;
-			incrementVals.y = incrementVals.y / 2;
+		else if (destinationY < y) {
+			incrementVals.x = -((destinationX - x) / (destinationY - y));
+			incrementVals.y = -1;
+			while (incrementVals.x < -3) {
+				incrementVals.x = incrementVals.x / 2;
+				incrementVals.y = incrementVals.y / 2;
+			}
 		}
 	}
 	return incrementVals;
